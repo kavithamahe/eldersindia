7a7d372a8041610642309974f4d76d98e8a8fd09
Govindaraj K <govindaraj.k@CLARITAZ.COM>
2017-04-01 17:40:01 +0530

    get login update

--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -52,10 +52,6 @@ token:any;
   editProfile(){
   	this.navCtrl.push(EditProfilePage,{profileData:this.profileData});
   }
-   public dashboardPage()
-  {
-    this.navCtrl.setRoot(DashboardPage);
-  }
   
   ionViewDidEnter(){
     this.loadMyProfile();
