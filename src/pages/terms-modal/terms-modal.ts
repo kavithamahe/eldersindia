50c64f65e1cebf2a74d50f516517536d35376ad0
samuel <samuel.r@quadrupleindia.com>
2017-04-28 19:03:21 +0530

    message updation

--- src/pages/terms-modal/terms-modal.ts
+++ src/pages/terms-modal/terms-modal.ts
@@ -13,28 +13,37 @@ import { ViewController,NavController, NavParams } from 'ionic-angular';
 })
 export class TermsModalPage {
 
-show_description:any;
-show_application_use:any;
-show_eligibility:any;
-show_user_accounts:any;
-show_renewal:any;
-show_personal_info:any;
-show_content:any;
-show_license:any;
-show_Intellectual:any;
-show_trade_marks:any;
-show_restrictions:any;
-show_termination:any;
-show_limitation:any;
-show_indemnification:any;
-show_miscellaneous:any;
-show_contact_us:any;
+description:any;
+show_description:any = false;
+// show_application_use:any;
+// show_eligibility:any;
+// show_user_accounts:any;
+// show_renewal:any;
+// show_personal_info:any;
+// show_content:any;
+// show_license:any;
+// show_Intellectual:any;
+// show_trade_marks:any;
+// show_restrictions:any;
+// show_termination:any;
+// show_limitation:any;
+// show_indemnification:any;
+// show_miscellaneous:any;
+// show_contact_us:any;
 show_details:any=null;
 
   constructor(public viewCtrl: ViewController,public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TermsModalPage');
+    this.description = 'These Terms of Use set out the terms and conditions under which individuals may use the website or mobile application and access the services offered by CareGuide Inc. ("CareGuide"). These Terms of Use comprise a legal agreement between CareGuide and you, the person accessing and using the website or mobile application ("You" or "User"). CareGuide owns, operates and maintains the CareGuide.com website at careguide.com, other subsidiary websites, including sitter.com (collectively referred to as "Website"), and the Sitter.com mobile application ("App"), Website and App collectively referred to as the "Platform". By downloading, accessing and/or using the Platform, you are deemed to have read Terms of Use and you agree to be bound by the Terms of Use which shall govern your access and use of the Platform and all applicable laws and regulations.';
+  }
+  toggleContent(){
+    if(this.show_description == false){
+      this.show_description = true;
+    }else{
+      this.show_description = false;
+    }
   }
   
   show(event){
