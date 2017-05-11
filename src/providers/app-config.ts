d7f0b7e039598f392806a3f56a37bb7f3ca43816
Gowindaraj <govindaraj.k@claritaz.com>
2017-05-11 17:13:36 +0530

    get bug fixes

--- src/providers/app-config.ts
+++ src/providers/app-config.ts
@@ -12,11 +12,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AppConfig {
 
-	 imageUrl="http://183.82.33.232:8097/"; 
-	 rooturl="http://183.82.33.232:8097/api/"; 
+//imageUrl="http://183.82.33.232:8097/"; 
+	// rooturl="http://192.168.1.20:8000/api/"; 
+	 
 
-  //imageUrl="http://192.168.1.20:8000/"; 
-  //rooturl=this.imageUrl+"api/";
+ //imageUrl="http://183.82.33.232:8097/"; 
+ imageUrl="http://52.91.174.4:8096/";
+ //imageUrl="http://192.168.1.20:8000/"; 
+ rooturl=this.imageUrl+"api/";
 
   constructor(public http: Http) {
   }
