c85b147b0b8e583ada99ad4111e727f3e9e3f004
venkat <venkat.r@claritaz.com>
2017-06-06 10:51:37 +0530

    get css

--- src/providers/app-config.ts
+++ src/providers/app-config.ts
@@ -12,7 +12,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AppConfig {
 
-  imageUrl="http://192.168.1.121:8000/"; 
+  imageUrl="http://52.91.174.4:8095/"; 
   rooturl=this.imageUrl+"api/";
 
   constructor(public http: Http) {
