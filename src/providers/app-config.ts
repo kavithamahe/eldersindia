86e3725e2c6508f19383e723f3d45dbf57306d94
kavitha <kavithamahe2@gmail.com>
2018-09-19 11:14:25 +0530

    kavi

--- src/providers/app-config.ts
+++ src/providers/app-config.ts
@@ -11,7 +11,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AppConfig {
-  imageUrl="http://qa.eldersindia.com/";
+  imageUrl="http://192.168.1.120:8000/";
   rooturl=this.imageUrl+"api/";
 
   constructor(public http: Http) {
